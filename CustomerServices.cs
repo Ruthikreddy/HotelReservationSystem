78f34c2643de51f3a8bdd65fb0ab3b6d48d9bc19
Ruthikreddy <ruthikreddy15@gmail.com>
2020-11-09 18:29:10 +0530

    [Ruthik] push for Best Rated Hotel

--- CustomerServices.cs
+++ CustomerServices.cs
@@ -35,5 +35,28 @@ namespace HotelReservation
                 Console.WriteLine("Best hotel for your stay is " + HotelType.RIDGEWOOD + " Rating: " + ridgewood.RATING + " Cost of stay: " + rateRidgewood);
             }
         }
+        /// <summary>
+        /// Finds best rated hotel
+        /// </summary>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        public void FindBestRatedHotel(string startDate, string endDate)
+        {
+            HotelType hotelType = HotelType.LAKEWOOD;
+            Hotel lakewood = new Hotel(hotelType);
+            double rateLakewood = lakewood.FindRate(startDate, endDate);
+            hotelType = HotelType.BRIDGEWOOD;
+            Hotel bridgewood = new Hotel(hotelType);
+            double rateBridgewood = bridgewood.FindRate(startDate, endDate);
+            hotelType = HotelType.RIDGEWOOD;
+            Hotel ridgewood = new Hotel(hotelType);
+            double rateRidgewood = ridgewood.FindRate(startDate, endDate);
+            if (lakewood.RATING > bridgewood.RATING && lakewood.RATING > ridgewood.RATING)
+                Console.WriteLine("Best hotel for your stay is " + HotelType.LAKEWOOD + " Rating: " + lakewood.RATING + " Cost of stay: " + rateLakewood);
+            if (bridgewood.RATING > lakewood.RATING && bridgewood.RATING > ridgewood.RATING)
+                Console.WriteLine("Best hotel for your stay is " + HotelType.BRIDGEWOOD + " Rating: " + bridgewood.RATING + " Cost of stay: " + rateBridgewood);
+            if (ridgewood.RATING > lakewood.RATING && ridgewood.RATING > bridgewood.RATING)
+                Console.WriteLine("Best hotel for your stay is " + HotelType.RIDGEWOOD + " Rating: " + ridgewood.RATING + " Cost of stay: " + rateRidgewood);
+        }
     }
 }
